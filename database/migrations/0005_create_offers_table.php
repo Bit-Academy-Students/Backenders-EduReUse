a8a1ad2b4171e3198cd72548e08bcb79a945164c
GIORGIO <gderijp@gmail.com>
2025-11-24 10:20:35 +0100

    add FK to -table

--- database/migrations/0005_create_offers_table.php
+++ database/migrations/0005_create_offers_table.php
@@ -9,12 +9,13 @@ $db->connect()->exec(
     "CREATE TABLE IF NOT EXISTS offers (
     id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     model varchar(100) NOT NULL,
-    staat_id varchar(25) NOT NULL,
+    staat_id INT NOT NULL,
     hoeveelheid INT NOT NULL,
     beschrijving TEXT NOT NULL,
     postcode varchar(7) NOT NULL,
     type_id INT NOT NULL,
     user_id INT NOT NULL,
+    FOREIGN KEY (staat_id) REFERENCES product_states(id),
     FOREIGN KEY (type_id) REFERENCES types(id),
     FOREIGN KEY (user_id) REFERENCES users(id))"
 );
