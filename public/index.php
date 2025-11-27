9a2c99262de882e0710a43f4bda278060e5509b8
Jenebi <153907694+JenebiDeel@users.noreply.github.com>
2025-11-27 15:37:42 +0100

    Tailwind toeevoegd

--- public/index.php
+++ public/index.php
@@ -18,29 +18,34 @@ $conn->query("USE edureuse");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="src/output.css">
     </head>
-        <body class="home-body">
-            <header class="home-header">
-                <div class="wrapper">
-                    <div class="home-logo">
+        <body>
+            <header class="bg-sky-300 p-3">
+                <div >
+                    <div class="flex flex-row-end">
                         <a href="index.php">LOGO</a>
                     </div>
-                    <nav class="home-nav">
-                        <a href="login.php">Login</a>
+                    <nav class="flex flex-row-reverse">
+                        <a class="flex bg-white rounded-lg w-30 justify-center items-center" href="login.php">Login</a>
                     </nav>
                 </div>
             </header>
 
-            <div class="banner">
-            <div class="wrapper">
-                <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, nostrum. Nemo ipsa omnis 
-                    illum vitae, laboriosam id iusto odio. Consequatur dolores quae culpa sequi earum officiis eveniet 
-                    ullam dolor accusantium?</p>
+            <div class="flex flex-row-end">
+                <div class="flex flex-col p-20">
+                    <h1 class="text-3xl font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                    <p class="mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, nostrum. Nemo ipsa omnis 
+                        illum vitae, laboriosam id iusto odio. Consequatur dolores quae culpa sequi earum officiis eveniet 
+                        ullam dolor accusantium?</p>
 
-                    <a href="register.php">Meld je nu aan</a>
-            </div>
+                    <a class="bg-sky-500 text-white rounded-md p-1.5 w-30 mt-5" href="register.php">Meld je nu aan</a>
+                </div>
+                <div class="pt-20 pr-20">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, nostrum. Nemo ipsa omnis 
+                        illum vitae, laboriosam id iusto odio. Consequatur dolores quae culpa sequi earum officiis eveniet 
+                        ullam dolor accusantium?</p>
+                </div>
             </div>
 
         </body>
--- public/register.php
+++ public/register.php
@@ -41,26 +41,44 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="src/output.css">
 </head>
 <body>
-    <div class="container">
-        <h2>Registreren</h2>
-        <form action="register.php" method="post">
-            <div class="form-group">
-                <label for="naam">School</label>
-                <input type="text" id="naam" name="naam" required>
+    <header class="bg-sky-300 p-6">
+                <div>
+                    <div class="flex flex-row items-center">
+                        <a href="index.php">LOGO</a>
+                    </div>
+                </div>
+            </header>
+    <div class="flex justify-center items-center bg-sky-100 h-screen">
+        
+        <div class="w-96 p-6 shadow-lg bg-white rounded-lg ">
+            <div class="flex justify-center items-center">
+                <h2 class="text-sky-600 font-bold text-3xl mb-10">Registreren</h2>
             </div>
-            <div class="form-group">
-                <label for="email">Email Address</label>
-                <input type="email" id="email" name="email" required>
-            </div>
-            <div class="form-group">
-                <label for="wachtwoord">wachtwoord</label>
-                <input type="wachtwoord" id="wachtwoord" name="wachtwoord" required>
-            </div>
-            <button type="submit" name="register">Registreren</button>
-        </form>
+            <form action="register.php" method="post" class="space-y-6">
+                <div>
+                    <label for="naam">School</label>
+                    <div class="bg-slate-100 mt-2 rounded-md shadow-xs">
+                    <input class="block w-full rounded-md py-1.5 px-3" type="text" id="naam" name="naam" required>
+                    </div>
+                </div>
+                <div>
+                    <label for="email">Email Address</label>
+                    <div class="bg-slate-100 mt-2 rounded-md shadow-xs">
+                        <input class="block w-full rounded-md py-1.5 px-3" type="email" id="email" name="email" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="wachtwoord">wachtwoord</label>
+                    <div class="bg-slate-100 mt-2 rounded-md shadow-xs">
+                    <input class="block w-full rounded-md py-1.5 px-3" type="text" id="wachtwoord" name="wachtwoord" required>
+                    </div>
+                </div>
+                <button class="flex w-full justify-center rounded-md bg-sky-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-sky-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-500" type="submit" name="register">Registreren</button>
+            </form>
+        </div>    
     </div>
 </body>
 </html>
