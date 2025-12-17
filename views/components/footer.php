61eb12e0eb5804ad9e8457bf61483822e8eaa5af
dymoreno milan <dymo_6266@hotmail.com>
2025-12-17 11:45:36 +0100

    homepage New2

---
+++ views/components/footer.php
@@ -0,0 +1,17 @@
+    <footer class="bg-blue-500 text-white py-8 mt-10 text-center">
+        <div class="flex flex-col items-end justify-center w-[90%] max-w-300 mx-auto">
+            <div class="text-right space-y-1">
+                <p class="font-bold text-lg">WailSalutem-Foundation</p>
+                <p>Tom lodewijklaan 20</p>
+                <p>1183WE Amstelveen</p>
+                <p>Nederland</p>
+                <p>T 0640690319</p>
+                <p>E info@wailsalutem-foundation.com</p>
+                <p>KVK 84381493</p>
+            </div>
+            
+            <div class="mt-6 text-right">
+                <p>Copyright 2025 © <a href="https://wailsalutem-foundation.com" class="hover:underline">WailSalutem-Foundation.com</a></p>
+            </div>
+        </div>
+    </footer>
